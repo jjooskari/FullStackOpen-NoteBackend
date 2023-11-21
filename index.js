47b7aa7fbb932fe68296927f0b8fab2baa15bbfd
jjooskari <jjooskari>
2023-11-21 14:11:22 +0200

    Ex 3.19

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number ) {
@@ -61,6 +61,7 @@ app.post('/api/persons/', (request, response) => {
     person.save().then( savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -92,9 +93,11 @@ const errorHandler = (error, request, response, next) => {
   
     // Incorrectly formatted id
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+        return response.status(400).send({ error: 'malformatted id' })
+    // Database validation error
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
